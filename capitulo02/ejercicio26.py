ef8fae8b126973a1d5846ab612bc277eaf50eed9
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2017-07-11 22:48:00 +0200

    Renombrar los archivos más sencillamente

--- capitulo02/ejercicio26.py
+++ capitulo02/ejercicio26.py
@@ -6,64 +6,7 @@ Ejercicio 26 del Capítulo 02.
 """
 
 from ast import literal_eval
-
-def es_iterable(objeto):
-    """
-    Comprobar si un objeto es de tipo iterable
-
-    Argumentos:
-        objeto: objeto a comprobar si es iterable.
-
-    Retorno:
-        True o False si el objeto es o no iterable.
-    """
-    return hasattr(objeto, "__getitem__") or hasattr(objeto, "__iter__")
-
-
-def obtener_dato(mensaje, evaluar=None, comprobar=None, fin=None):
-    """
-    Leer cadenas desde el teclado hasta que una de ellas cumpla alguna de estas
-    condiciones:
-    - La cadena sea una de las cadenas de finalización.
-    - La cadena, una vez evaluada en caso de evaluarse, pase una comprobación.
-
-    Argumentos:
-        mensaje: mensaje a ser mostrado antes de introducir el dato por teclado.
-        evaluar: función usada para evaluar la cadena introducida y obtener
-            el tipo de dato deseado. Debe lanzar una excepción ValueError si
-            la cadena a evaluar no tiene el formato correcto. En caso de None
-            la cadena leída por teclado no se evalúa.
-        comprobar: función que comprueba si el dato introducido (evaluado en
-            caso de evaluarse) es correcto. Si es None, no se realiza ninguna
-            comprobación del dato.
-            Debe devolver True si el dato es correcto o False en caso contrario.
-        fin: cadena o lista de cadenas de finalización que interrumpe la lectura
-            por teclado. Si es None no se realiza ninguna comprobación de
-            finalización de la cadena introducida por teclado.
-
-    Retorno:
-        Los posibles valores devueltos son:
-        - None si la cadena leída es una cadena de finalización.
-        - Dato introducido por teclado ya evaluado y pasada la comprobación.
-    """
-    while True:
-        dato = input(mensaje).strip()
-        if fin is not None and \
-           (es_iterable(fin) and str.lower(dato) in map(str.lower, fin) or \
-            not es_iterable(fin) and str.lower(dato) == str.lower(fin)):
-            return None
-
-        if evaluar is not None:
-            try:
-                dato = evaluar(dato)
-            except ValueError:
-                print("ERROR: fallo al evaluar la cadena introducida.")
-                continue
-
-        if comprobar is None or comprobar(dato):
-            return dato
-
-        print("ERROR: el dato está en formato incorrecto.")
+from util import es_iterable, obtener_dato
 
 
 def es_punto(punto):
